9d231edba42cb395d9da6dd7c5ab9efa2d7f42b1
YaokaiYang-assaultmaster <yxy586@case.edu>
2017-03-23 18:47:05 -0400

    Add setup files for module installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name = 'PortScanner',
+	version = '0.1',
+	description = 'Python Port Scanner',
+	packages = ['PortScanner'],
+	include_package_data = True,
+)
